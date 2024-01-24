81ca86c751a25cf842b9772c8af5e031000a699a
zxchope <80282337+schlimmsten@users.noreply.github.com>
2024-01-24 05:21:03 +0300

    Lab3_fixed

--- fixed.py
+++ fixed.py
@@ -1,64 +1,58 @@
-BOOKS_DATABASE = [
-    {
-        "id": 1,
-        "name": "test_name_1",
-        "pages": 200,
-    },
-    {
-        "id": 2,
-        "name": "test_name_2",
-        "pages": 400,
-    }
-]
-
-
-# TODO написать класс Book
 class Book:
-    def __init__(self, id_, name, pages):
-        self.id_ = id_
-        self.name = name
-        self.pages = pages
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
 
-    def __str__(self):
-        return f'Книга "{self.name}"'
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
 
     def __repr__(self):
-        return f"Book(id_={self.id_}, name='{self.name}', pages={self.pages})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
 
-class Library:
+class PaperBook(Book):
+    def __init__(self, name: str, author: str):
+        super().__init__(name, author)
+        self._pages = 0
 
-    def __init__(self, books = None):
-        if books is None:
-            books = list()
-            self.books = books
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if pages > 0 and isinstance(pages, int):
+            self._pages = pages
         else:
-            self.books = books
+            print("Incorrect number of pages")
 
-    def get_next_book_id(self):
-        if len(self.books) == 0:
-            return 1
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Страниц {self._pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str):
+        super().__init__(name, author)
+        self._duration = 0
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: int) -> None:
+        if duration > 0 and isinstance(duration, float):
+            self._duration = duration
         else:
-            return self.books[-1].id_+1 #какой то id? последней
+            print("Incorrect number of pages")
 
-    def get_index_by_book_id(self, name):
-        try:
-            for index, book in enumerate(self.books):
-                if book.id_ == name:
-                    return index
-        except ValueError:
-            print("Книги с запрашиваемым id не существует")
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Длительность {self._duration}"
 
 
-
-if __name__ == '__main__':
-    empty_library = Library()  # инициализируем пустую библиотеку
-    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
-
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
-    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
-    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
-
-    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
